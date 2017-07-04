3182aee12967a6a0a3ac8baf46c1d897a5219dd7
Zach Foster <notzachfoster@gmail.com>
2017-07-04 09:49:50 -0700

    add proper functions dir

---
+++ functions/product-war-bot/index.js
@@ -0,0 +1,49 @@
+/**
+ * Requires (Custom Modules)
+ */
+var helper = require('../../helper');
+var commands = require('../../commands');
+
+/**
+ * Main Lambda function
+ *
+ * @param {object} event AWS Lambda uses this parameter to pass in event data to the handler.
+ * @param {object} context AWS Lambda uses this parameter to provide your handler the runtime information of the Lambda function that is executing.
+ *
+ * @return {object} Request Promise
+ */
+exports.handler = function(event, context, callback) {
+  console.log(event)
+  var processCommand = processCommands(event);
+
+  processCommand.then(function(response) {
+    callback(null, response);
+  }).catch(function(error) {
+    console.log(error);
+    callback(error.message);
+  });
+
+  return processCommand;
+};
+
+/**
+ * Process Commands
+ *
+ * @param {object} event AWS Lambda Event
+ *
+ * @return {object} Request Promise
+ */
+function processCommands(event) {
+  if (event && event.text && event.trigger_word) {
+    if (!commands[event.trigger_word]) {
+      return commands.error('Invalid Command');
+    }
+
+    var command = event.trigger_word.toLowerCase();
+    var commandArguments = helper.parseCommand(event.text.trim());
+
+    return commands[command](commandArguments[command]);
+  }
+
+  return commands.error('Event not specified');
+}
